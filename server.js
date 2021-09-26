924e5d4f42275c0c379b861cf1bee35a12c5268d
Dylan Shoemaker <me@dylanshoemaker.com>
2021-09-25 17:32:45 -0700

    Revert "set timeout function to have people relog back in after 2 hours"
    This reverts commit 335e7755cf6a0baf6eea37016604b559960e6273.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: { expires: 7200000 },
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
